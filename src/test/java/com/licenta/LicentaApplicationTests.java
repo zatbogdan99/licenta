52a2fc183a89583dacd05aa42916ce86c31bd32d
Bogdan Zat <zatbogdan99@gmail.com>
2022-03-05 00:46:15 +0200

    Initial commit

---
+++ src/test/java/com/licenta/LicentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenta;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicentaApplicationTests {
+	@Test
+	public void initialTest() {
+		Assertions.assertTrue(true);
+	}
+}
